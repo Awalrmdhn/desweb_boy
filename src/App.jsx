180f5aef1d7a68524c12fd0b67adf298e8016f92
Awalrmdhn <11221067@student.itk.ac.id>
2024-11-05 00:43:53 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-3xl font-bold text-blue-500">Hello, React + Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;

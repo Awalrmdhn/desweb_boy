27ab0dfe65d60ea65e39e7298df82cfa88766ab2
Awalrmdhn <11221067@student.itk.ac.id>
2024-11-05 03:15:07 +0800

    uda

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './component/navbar'; // Import Navbar
 import HomePage from './pages/Homepage'; // Import HomePage
 import DropPoint from './pages/DropPoint';
+import Kursus from './pages/Kursus';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} /> {/* Direct root path to HomePage */}
         <Route path="/DropPoint" element={<DropPoint />} /> {/* Direct DropPoint path to DropPoint */}
+        <Route path="/Kursus" element={<Kursus />} />
       </Routes>
     </div>
   );
